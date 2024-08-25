f6702ae137de771c08820ba0e186d52d1f0796ed
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-25 15:24:14 +0530

    feat(dashboard): enhance Dashboard with grid layout for search, pet list, and details
    - Added `SearchForm`, `ContentBlock` components to the Dashboard page
    - Implemented responsive grid layout with three columns for search, pet list, and pet details sections
    - Applied conditional row and column spans for better layout on different screen sizes

--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import Branding from "@/components/branding";
+import ContentBlock from "@/components/content-block";
+import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
 const page = () => {
@@ -7,6 +9,20 @@ const page = () => {
       <div className="flex items-center justify-between text-white py-8">
         <Branding />
         <Stats />
+      </div>
+      <div
+        className="grid md:grid-cols-3 md:grid-rows-[45px_1fr] 
+      grid-rows-[45px_300px_500px] gap-4 md:h-[600px]"
+      >
+        <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1">
+          <SearchForm />
+        </div>
+        <div className=" md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
+          <ContentBlock>petlist</ContentBlock>
+        </div>
+        <div className="md:row-start-1 md:row-span-full md:col-start-2 md:col-span-full">
+          <ContentBlock>petdetails</ContentBlock>
+        </div>
       </div>
     </main>
   );
