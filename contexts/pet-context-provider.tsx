01cf6e48945a3c3c7fbd5319bcae7be78b833c43
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-26 17:08:41 +0530

    feat(context): add derived state and event handlers to PetContextProvider
    - Added `selectedPet` derived state to retrieve the currently selected pet
    - Implemented `handleChangeSelectedPetId` event handler to update the selected pet ID
    - Enhanced `PetContextProvider` to manage and expose the selected pet and ID change functionality

--- contexts/pet-context-provider.tsx
+++ contexts/pet-context-provider.tsx
@@ -16,7 +16,16 @@ export const PetContext = createContext<TPetContext | null>(null);
 
 const PetContextProvider = ({ children, data }: Props) => {
   const [pets, setPets] = useState(data);
-  const [selectedPetId, setSelectPetId] = useState(null);
+  const [selectedPetId, setSelectPetId] = useState<string | null>(null);
+
+  // derived state
+  const selectedPet = pets.find((pet) => pet.id === selectedPetId);
+
+  //event handlers
+  const handleChangeSelectedPetId = (id: string) => {
+    setSelectPetId(id);
+  };
+
   return (
     <PetContext.Provider value={{ pets, selectedPetId }}>
       {children}
