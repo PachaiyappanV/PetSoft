94c99c6cba32b17aefd43bb8db6a4000d17f7676
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-26 17:31:54 +0530

    feat(account): enhance account page with styling and user info display
    - Added `H1` component for the page title "Your Account"
    - Wrapped content in `ContentBlock` for consistent styling
    - Displayed logged-in user's name "pachaiyappan" in the account page

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -1,5 +1,15 @@
+import ContentBlock from "@/components/content-block";
+import H1 from "@/components/h1";
+
 const page = () => {
-  return <div>Account page</div>;
+  return (
+    <main>
+      <H1 className="my-8 text-white">Your Account</H1>
+      <ContentBlock className="h-[500px] flex flex-col gap-3 justify-center items-center">
+        <p>Logged in as pachaiyappan</p>
+      </ContentBlock>
+    </main>
+  );
 };
 
 export default page;
--- components/h1.tsx
+++ components/h1.tsx
@@ -7,7 +7,11 @@ type Props = {
 };
 
 const H1 = ({ children, className }: Props) => {
-  return <h1 className={cn("font-medium text-2xl leading-6")}>{children}</h1>;
+  return (
+    <h1 className={cn("font-medium text-2xl leading-6", className)}>
+      {children}
+    </h1>
+  );
 };
 
 export default H1;
