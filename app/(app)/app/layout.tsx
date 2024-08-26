3daf834430efa22d80a7e445ade80c6f6d385677
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-26 17:02:40 +0530

    feat(pet-list): add search functionality to filter pets
    - Integrated `useSearchContext` to access search query
    - Filtered pets based on search query to display matching results
    - Updated `PetList` component to reflect search input changes

--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -2,6 +2,7 @@ import AppFooter from "@/components/app-footer";
 import AppHeader from "@/components/app-header";
 import BackgroundPattern from "@/components/background-pattern";
 import PetContextProvider from "@/contexts/pet-context-provider";
+import SearchContextProvider from "@/contexts/search-context-provider";
 import { ReactNode } from "react";
 
 type Props = {
@@ -23,7 +24,9 @@ const Layout = async ({ children }: Props) => {
       <BackgroundPattern />
       <div className="max-w-[1050px] flex flex-col mx-auto min-h-screen">
         <AppHeader />
-        <PetContextProvider data={data}>{children}</PetContextProvider>
+        <SearchContextProvider>
+          <PetContextProvider data={data}>{children}</PetContextProvider>
+        </SearchContextProvider>
 
         <AppFooter />
       </div>
--- components/pet-list.tsx
+++ components/pet-list.tsx
@@ -1,13 +1,18 @@
 "use client";
-import { usePetContext } from "@/lib/hooks";
+import { usePetContext, useSearchContext } from "@/lib/hooks";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 const PetList = () => {
   const { pets, handleChangeSelectedPetId, selectedPetId } = usePetContext();
+  const { searchQuery } = useSearchContext();
+
+  const filterdPets = pets.filter((pet) =>
+    pet.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <ul className="bg-white border-b border-black/10">
-      {pets.map((pet) => (
+      {filterdPets.map((pet) => (
         <li key={pet.id}>
           <button
             onClick={() => handleChangeSelectedPetId(pet.id)}
