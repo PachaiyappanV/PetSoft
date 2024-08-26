60fdb4c4e74bba0297c168c8acde569c6db9ff81
PachaiyappanV <pachaiyappan394@gmail.com>
2024-08-26 16:36:53 +0530

    feat(context): create SearchContext for managing search query
    - Added `SearchContext` to handle search query state across components
    - Implemented `SearchContextProvider` to provide search query and update functionality
    - Introduced `handleChangeSearchQuery` method to update the search query

---
+++ contexts/search-context-provider.tsx
@@ -0,0 +1,27 @@
+import { ReactNode, createContext, useState } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+type TSearchContext = {
+  searchQuery: string;
+  handleChangeSearchQuery: (newValue: string) => void;
+};
+
+export const SearchContext = createContext<TSearchContext | null>(null);
+
+const SearchContextProvider = ({ children }: Props) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleChangeSearchQuery = (newValue: string) => {
+    setSearchQuery(newValue);
+  };
+  return (
+    <SearchContext.Provider value={{ searchQuery, handleChangeSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchContextProvider;
