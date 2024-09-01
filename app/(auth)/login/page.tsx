686ea1ef95ad08ea225fb544c0fa8f4a3942e54f
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-01 15:36:20 +0530

    feat(auth): create login page
    - Added a new `Log In` page with a form component for user authentication.
    - Used `AuthForm` component to handle the login process.
    - Included a link to the `Sign Up` page for users who do not have an account.
    - Styled the page with consistent typography and spacing.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,20 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 const page = () => {
-  return <div>login</div>;
+  return (
+    <main>
+      <H1 className="text-center mb-5">Log In</H1>
+      <AuthForm />
+      <p className=" mt-6 text-sm text-zinc-500">
+        No account yet?{" "}
+        <Link href="/signup" className="font-medium">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
 };
 
 export default page;
