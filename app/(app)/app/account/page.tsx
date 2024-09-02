000a8a0c5d3ce6824bc571cf1c937669f79b3abb
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-02 08:44:52 +0530

    feat(account): secure account page with user authentication
    - Added dynamic authentication check using `auth` to verify user session.
    - Redirected unauthenticated users to the login page.
    - Displayed the authenticated user's email dynamically on the account page.

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -1,12 +1,17 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
-
-const page = () => {
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+const page = async () => {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
       <ContentBlock className="h-[500px] flex flex-col gap-3 justify-center items-center">
-        <p>Logged in as pachaiyappan</p>
+        <p>Logged in as {session.user.email}</p>
       </ContentBlock>
     </main>
   );
