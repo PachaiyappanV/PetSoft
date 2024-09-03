1befb746e7c224e2a5a64565eaf1c5957d73bb7e
PachaiyappanV <pachaiyappan394@gmail.com>
2024-09-03 12:26:35 +0530

    feat(sign-out-btn): enhance sign-out functionality with loading state
    - Added `useTransition` to manage the sign-out process asynchronously.
    - Disabled the button while the sign-out action is pending to prevent multiple submissions.
    - Improved user experience by indicating when the sign-out process is in progress.

--- components/sign-out-btn.tsx
+++ components/sign-out-btn.tsx
@@ -1,9 +1,22 @@
 "use client";
 import { logOut } from "@/actions/actions";
+import { useTransition } from "react";
 import { Button } from "./ui/button";
 
 const SignOutBtn = () => {
-  return <Button onClick={async () => await logOut()}>Sign out</Button>;
+  const [isPending, startTransition] = useTransition();
+  return (
+    <Button
+      onClick={async () => {
+        startTransition(async () => {
+          await logOut();
+        });
+      }}
+      disabled={isPending}
+    >
+      Sign out
+    </Button>
+  );
 };
 
 export default SignOutBtn;
