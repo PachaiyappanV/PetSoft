845c5af3b127e429b785bba84ae737701fe0daab
PachaiyappanV <pachaiyappan394@gmail.com>
2025-01-23 00:38:25 +0530

    chore(footer): update copyright year

--- components/app-footer.tsx
+++ components/app-footer.tsx
@@ -2,7 +2,7 @@ const AppFooter = () => {
   return (
     <footer className="mt-auto border-t border-black/20 py-5">
       <small className="opacity-50">
-        &copy; 2030 Pachaiyappan. All rights reserved.
+        &copy; 2025 Pachaiyappan. All rights reserved.
       </small>
     </footer>
   );
